05a2445d316371585f50dd0c176d3ad46aeea55c
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-05-03 21:53:51 +0200

    feat: create SquareStatus enum and Square class

--- src/battleship/Square.java
+++ src/battleship/Square.java
@@ -5,7 +5,22 @@ public class Square {
 	private int Y;
 	private SquareStatus status;
 
-	public SquareStatus getCharacter() {
+	public Square(int x, int y, SquareStatus status) {
+		X = x;
+		Y = y;
+		this.status = status;
+	}
+
+	public String getCharacter() {
 		return null; // empty, ship, hit, missed - graphical representation
+	}
+	// do we need getter and setter?
+	// difference getStataus vs getCharacter?
+	public SquareStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(SquareStatus status) {
+		this.status = status;
 	}
 }
--- src/battleship/SquareStatus.java
+++ src/battleship/SquareStatus.java
@@ -2,5 +2,15 @@ package battleship;
 
 public enum SquareStatus {
 	// (empty, ship, hit, missed) unicode character
+	EMPTY("."),
+	SHIP("S"),
+	HIT("H"),
+	MISSED("X");
+
+	String status;
+
+	SquareStatus(String status) {
+		this.status = status;
+	}
 
 }
