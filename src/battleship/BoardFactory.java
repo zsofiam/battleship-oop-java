aee35eb75c38bbd466269ba905a55b5ca504face
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-05-04 14:08:02 +0200

    feat: get starting coordinates input

--- src/battleship/BoardFactory.java
+++ src/battleship/BoardFactory.java
@@ -6,6 +6,7 @@ public class BoardFactory {
 
 	private Board board;
 	private Random random;
+	private Input input;
 
 	public void randomPlacement() {
 
@@ -70,5 +71,33 @@ public class BoardFactory {
 
 	public void manualPlacement() {
 
+		int boardSize = getBoardSize();
+		if (boardSize > 5) {
+			placeOnBoardManual(ShipType.CARRIER);
+		} if (boardSize > 15) {
+			placeOnBoardManual(ShipType.CRUISER);
+		} if (boardSize > 25) {
+			placeOnBoardManual(ShipType.BATTLESHIP);
+		} if (boardSize > 35) {
+			placeOnBoardManual(ShipType.SUBMARINE);
+		} if (boardSize > 45) {
+			placeOnBoardManual(ShipType.DESTROYER);
+		}
+	}
+
+	private void placeOnBoardManual(ShipType shipType) {
+		boolean isPlacementOk = false;
+		int row = 0;
+		int col = 0;
+		String[] directions = new String[]{"h", "v"};
+		String direction = null;
+		int[] startPosition = new int[2];
+		int length = shipType.length;
+		while (!isPlacementOk){
+			startPosition = input.getStartCoordinates(shipType);
+			direction = input.getShipDirection();
+			isPlacementOk = board.isPlacementOk(startPosition, direction, length);
+		}
+		placeOnBoard(shipType.length, new int[]{row,col}, direction);
 	}
 }
--- src/battleship/Input.java
+++ src/battleship/Input.java
@@ -1,6 +1,34 @@
 package battleship;
 
+import java.util.Scanner;
+
 public class Input {
-	// gather every input
+    Scanner scanner = new Scanner(System.in);
+
+    public int[] getStartCoordinates(ShipType shipType) {
+        boolean validInput = false;
+        int row = 0;
+        int col = 0;
+        while (!validInput){
+            System.out.println(String.format("Please enter %s (length: %d) coordinates:%n", shipType.name(),shipType.length));
+            String coordinates = scanner.nextLine();
+            try {
+                row = Integer.parseInt(coordinates.substring(0,1));
+                col = Integer.parseInt(coordinates.substring(1));
+            }
+            catch (NumberFormatException e){
+                System.out.println("Coordinates are not valid!");
+                continue;
+            }
+            validInput = true;
+        }
+        
+        return new int[]{row, col};
+    }
+
+    public String getShipDirection() {
+        return null;
+    }
+    // gather every input
 	// validate every input
 }
