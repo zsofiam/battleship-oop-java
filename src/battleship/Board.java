7c5dc3fb5880c471e9b46b223787439c196a9b04
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-05-04 09:57:55 +0200

    feat: add Board constructor to build board

--- src/battleship/Board.java
+++ src/battleship/Board.java
@@ -3,6 +3,17 @@ package battleship;
 public class Board {
 	private Square[][] ocean;
 
+	public Board(int row, int col) {
+		ocean = new Square[row][col];
+		fillBoard(row, col);
+	}
+	private void fillBoard(int row, int col) {
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				ocean[i][j] = new Square(i, j, SquareStatus.EMPTY);
+			}
+		}
+	}
 	public boolean isPlacementOk() {
 		return true;
 	}
