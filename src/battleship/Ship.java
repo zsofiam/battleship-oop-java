20713c47a1596a028bc0c452e786ffd68b2e046e
Farkas Timot <farkas.timot@gmail.com>
2021-05-04 14:06:46 +0200

    feat: added constructors to the ship class, and made some basic functionality related to the squares list

--- src/battleship/Ship.java
+++ src/battleship/Ship.java
@@ -1,7 +1,23 @@
 package battleship;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Ship {
 	private List<Square> squares;
+
+	public Ship() {
+		//Initialize empty squares
+		this.squares = new LinkedList<>();
+	}
+
+	public Ship(List<Square> squares) {
+		//We don't need to initialize, the argument is
+		this.squares = squares;
+	}
+
+	//Add scares one-by-one in case we inited with Ship()
+	public void addSquare(Square square) {
+		squares.add(square);
+	}
 }
