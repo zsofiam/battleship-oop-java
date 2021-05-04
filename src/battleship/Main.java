009cbdcc4c3b7ce27d778d6584f431a2eab317d1
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-05-04 10:49:20 +0200

    refactor: delete testing code from main

--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -5,14 +5,6 @@ public class Main {
 		int row = 10;
 		int col = 10;
 		Board board = new Board(row, col);
-		int[] coordinates = {6, 6};
-		String direction = "V";
-		int length = 5;
-		System.out.println(board.isPlacementOk(coordinates, direction, length)); // true
-		int[] coordinates2 = {8, 8};
-		System.out.println(board.isPlacementOk(coordinates2, direction, length)); // false
-		String direction2 = "H";
-		System.out.println(board.isPlacementOk(coordinates, direction2, length)); // true
-		System.out.println(board.isPlacementOk(coordinates2, direction2, length)); // false
+
 	}
 }
