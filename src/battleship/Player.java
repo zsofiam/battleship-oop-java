3d88c5ef58f4c4f810ee7610f69576e77c8a929f
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-05-05 11:35:00 +0200

    feat: handle shooting enemy based on valid input

--- src/battleship/Player.java
+++ src/battleship/Player.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 public class Player {
 	private List<Ship> ships;
-	public void handleShot() {
-		// handle shot
+
+	public void handleShot(Square[][] enemyBoard, int[] coordinates) {
+		int row = coordinates[0];
+		int col = coordinates[1];
+		if (enemyBoard[row][col].getStatus() == SquareStatus.SHIP) {
+			enemyBoard[row][col].setStatus(SquareStatus.HIT);
+		} else {
+			enemyBoard[row][col].setStatus(SquareStatus.MISSED);
+		}
 	}
 
 	public boolean isAlive() {
